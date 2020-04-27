d350997a9d7295061f9f936d18fe0703fd1528d0
makenzie-ops <laurenmakenzie0@gmail.com>
2020-04-27 13:31:59 +0300

    initial commit

---
+++ app/request.py
@@ -0,0 +1,14 @@
+from app import app
+import urllib.request,json
+from .models import news
+
+News = news.News
+
+
+# Getting api key
+api_key = app.config['NEWS_API_KEY']
+
+
+# Getting the news headlines base url
+base_url = app.config["NEWS_HEADLINES_API_BASE_URL"]
+
---
+++ config.py
@@ -0,0 +1,26 @@
+class Config:
+    '''
+    General configuration parent class
+    '''
+    NEWS_HEADLINES_API_BASE_URL ='http://newsapi.org/v2/top-headlines?country={}&apiKey={}'
+    NEWS_SOURCES_API_BASE_URL = 'http://newsapi.org/v2/top-headlines?country={}&apiKey={}'
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True

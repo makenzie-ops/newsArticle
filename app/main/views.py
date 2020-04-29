67ed1c5319ddb54194e7885b9a21a265c3310976
makenzie-ops <laurenmakenzie0@gmail.com>
2020-04-29 13:26:07 +0300

    add bootstrap styles

--- app/main/views.py
+++ app/main/views.py
@@ -11,7 +11,7 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    # Getting latest news
+   
     latest_news = get_Source_news()
     headlines = get_headlines()
     
